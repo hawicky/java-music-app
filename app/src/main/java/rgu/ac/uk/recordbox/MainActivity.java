800827a03e291d4e15a26928f5a98912998bf9de
Saran <james.warrender1@btinternet.com>
2019-10-15 22:20:33 +0100

    created home screen with buttons,text and logo with navigation bar.

--- app/src/main/java/rgu/ac/uk/recordbox/MainActivity.java
+++ app/src/main/java/rgu/ac/uk/recordbox/MainActivity.java
@@ -2,7 +2,13 @@ package rgu.ac.uk.recordbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Logo Text colorSpan
+        //select the word box and make it orange
+        String app = getApplicationInfo().loadLabel(getPackageManager()).toString();
+        SpannableString ss = new SpannableString(app);
+        ForegroundColorSpan fcsOrange = new ForegroundColorSpan(getResources().getColor(R.color.colorOrange));
+        ss.setSpan(fcsOrange,6,9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        setTitle(ss);
+
     }
 }
