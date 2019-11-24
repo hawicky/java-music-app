b75f9ee47bcba8e5c33b49fea18752434ff49117
Saran <james.warrender1@btinternet.com>
2019-11-24 17:28:34 +0000

    Refactor Navigation Pages to use Fragment

--- app/src/main/java/rgu/ac/uk/recordbox/MainActivity.java
+++ app/src/main/java/rgu/ac/uk/recordbox/MainActivity.java
@@ -1,13 +1,20 @@
 package rgu.ac.uk.recordbox;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,5 +32,35 @@ public class MainActivity extends AppCompatActivity {
         ss.setSpan(fcsOrange,6,9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         setTitle(ss);
 
+        //bottom nav
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, new home_frag()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()){
+                        case R.id.home_nav:
+                            selectedFragment = new home_frag();
+                            break;
+                        case R.id.search_nav:
+                            selectedFragment = new search_frag();
+                            break;
+                        case R.id.contact_Nav:
+                            selectedFragment = new contact_frag();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frag_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+
+
 }
