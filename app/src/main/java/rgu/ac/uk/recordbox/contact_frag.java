c2bc704483a80b1aaabec59f0d3dcd10bedf70e7
Calum <calumw19@icloud.com>
2019-12-01 14:13:56 +0000

    Comment and sting changes

--- app/src/main/java/rgu/ac/uk/recordbox/contact_frag.java
+++ app/src/main/java/rgu/ac/uk/recordbox/contact_frag.java
@@ -38,15 +38,15 @@ public class contact_frag extends Fragment implements View.OnClickListener
     {
         if(v.getId() == R.id.button)
         {
-            // Get the persons name
+            // Get the persons name from the message box on contact page
             EditText nameBox = getView().findViewById(R.id.nameBox);
             String name = nameBox.getText().toString();
 
-            // Get the persons email
+            // Get the persons email from the message box on contact page
             EditText emailBox = getView().findViewById(R.id.emailBox);
             String email = emailBox.getText().toString();
 
-            // Get the persons message
+            // Get the persons message from the message box on contact page
             EditText messageBox = getView().findViewById(R.id.messageBox);
             String message = messageBox.getText().toString();
 
@@ -54,17 +54,21 @@ public class contact_frag extends Fragment implements View.OnClickListener
             message = message.substring(0, 1).toUpperCase() + message.substring(1).toLowerCase();
 
             // Create an email intent to open external email app
+            //Fill in the to adress to the teams own email
             Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "team14coursework@gmail.com", null));
             emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, email);
+            //Convert users name to uppercase
             emailIntent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml(getString(R.string.emailMessage,message,name.toUpperCase())));
+            //Set the subject to variable email subject
             emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject));
 
             PackageManager packageManager = getActivity().getPackageManager();
             boolean isIntentSafe = emailIntent.resolveActivity(packageManager) != null;
 
-            // Start the email app if it is safe otherwise tell them there's no email app
+            // Start the email app if it is safe
             if (isIntentSafe)
                 startActivity(emailIntent);
+            //Tell user via a toast that no email app is installed
             else
                 Toast.makeText(getActivity(), getString(R.string.no_email_app), Toast.LENGTH_SHORT).show();
         }
