b4822bf497fd8fbf12248b82d6239819edc0dda4
Lewis <lewis.hawick@gmail.com>
2019-12-03 13:46:02 +0000

    added album list recyclerview to home page, with home page design tweaks

---
+++ app/src/main/java/rgu/ac/uk/recordbox/ExampleAdapter.java
@@ -0,0 +1,54 @@
+package rgu.ac.uk.recordbox;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
+    private ArrayList<AlbumRecyclerView> mRecyclerList;
+
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public TextView mTextView1;
+        public TextView mTextView2;
+
+        public ExampleViewHolder(View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imageView);
+            mTextView1 = itemView.findViewById(R.id.textView);
+            mTextView2 = itemView.findViewById(R.id.textView2);
+        }
+    }
+
+    public ExampleAdapter(ArrayList<AlbumRecyclerView> recyclerList){
+        mRecyclerList = recyclerList;
+    }
+
+    @Override
+    public ExampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.album_recycler_view, parent, false);
+        ExampleViewHolder evh = new ExampleViewHolder(v); //evh = example view holder
+        return evh;
+
+    }
+
+    @Override
+    public void onBindViewHolder(ExampleViewHolder holder, int position) {
+        AlbumRecyclerView currentItem = mRecyclerList.get(position);
+
+        holder.mImageView.setImageResource(currentItem.getImageResource());
+        holder.mTextView1.setText(currentItem.getText1());
+        holder.mTextView2.setText(currentItem.getText2());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecyclerList.size();
+    }
+}
--- app/src/main/java/rgu/ac/uk/recordbox/home_frag.java
+++ app/src/main/java/rgu/ac/uk/recordbox/home_frag.java
@@ -5,14 +5,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class home_frag extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        //recycler view
+        ArrayList<AlbumRecyclerView> exampleList = new ArrayList<>();
+        exampleList.add(new AlbumRecyclerView(R.drawable.ic_album, "Post Malone", "Stoney"));
+        exampleList.add(new AlbumRecyclerView(R.drawable.ic_album, "21 Savage", "Issa Album"));
+        exampleList.add(new AlbumRecyclerView(R.drawable.ic_album, "Twenty One Pilots", "Trench"));
+        exampleList.add(new AlbumRecyclerView(R.drawable.ic_album, "Ed Sheeran", "X"));
+        exampleList.add(new AlbumRecyclerView(R.drawable.ic_album, "Line 9", "Line 6"));
+        exampleList.add(new AlbumRecyclerView(R.drawable.ic_album, "Line 5", "Line 6"));
+        exampleList.add(new AlbumRecyclerView(R.drawable.ic_album, "Line 5", "Line 6"));
+        exampleList.add(new AlbumRecyclerView(R.drawable.ic_album, "Line 5", "Line 6"));
+        exampleList.add(new AlbumRecyclerView(R.drawable.ic_album, "Line 5", "Line 6"));
+        exampleList.add(new AlbumRecyclerView(R.drawable.ic_album, "Line 5", "Line 6"));
+
+        mRecyclerView = rootView.findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mAdapter = new ExampleAdapter(exampleList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        return rootView;
     }
 }
