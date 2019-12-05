6f60296d95cedf0e9eb9ad762b762fe2ba0e8642
Calum <calumw19@icloud.com>
2019-12-05 12:39:37 +0000

    Adding shared name save in contact

--- app/src/main/java/rgu/ac/uk/recordbox/contact_frag.java
+++ app/src/main/java/rgu/ac/uk/recordbox/contact_frag.java
@@ -1,6 +1,7 @@
 package rgu.ac.uk.recordbox;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
@@ -11,6 +12,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,6 +24,8 @@ import androidx.fragment.app.Fragment;
 import java.util.Objects;
 
 import rgu.ac.uk.recordbox.R;
+
+import static android.content.Context.MODE_PRIVATE;
 
 //create contact frag class that will inherit from Fragment superclass and implements Viewclick listener
 public class contact_frag extends Fragment implements View.OnClickListener
@@ -36,6 +41,20 @@ public class contact_frag extends Fragment implements View.OnClickListener
         send_button = v.findViewById(R.id.button);
         send_button.setOnClickListener(this);
 
+        final Switch switchButton = getView().findViewById(R.id.nameSave);
+        final TextView nameBox = getView().findViewById(R.id.nameBox);
+
+        SharedPreferences sp = this.getActivity().getSharedPreferences("contactname", MODE_PRIVATE);
+        String sharedPrefName = sp.getString("name", "");
+        String sharedPrefSwitch = sp.getString("switch", "");
+
+        nameBox.setText(sharedPrefName);
+
+        if(!sharedPrefSwitch.equals(""))
+            switchButton.setChecked(true);
+        else
+            switchButton.setChecked(false);
+
         return v;
     }
 
@@ -43,7 +62,12 @@ public class contact_frag extends Fragment implements View.OnClickListener
     @Override
     public void onClick(View v)
     {
-        if(v.getId() == R.id.button) {
+        if(v.getId() == R.id.button)
+        {
+            // Open contactname shared preferences
+            SharedPreferences.Editor editor = getContext().getSharedPreferences("contactname", MODE_PRIVATE).edit();
+
+
             // Get the persons name from the message box on contact page
             EditText nameBox = Objects.requireNonNull(getView()).findViewById(R.id.nameBox);
             String name = nameBox.getText().toString();
@@ -59,6 +83,26 @@ public class contact_frag extends Fragment implements View.OnClickListener
             // Get the persons message from the message box on contact page
             EditText messageBox = getView().findViewById(R.id.messageBox);
             String message = messageBox.getText().toString();
+
+            // Get the switch button on the page
+            Switch saveName = getView().findViewById(R.id.nameSave);
+
+            // if the switch button is checked
+            if(saveName.isChecked())
+            {
+                // put the name and switch status in shared preferences
+                editor.putString("name", name).apply();
+                editor.putString("switch", "enabled").apply();
+            }
+
+            // if the switch button is unchecked
+            else
+            {
+                // put the name as blank and switch status as blank in shared preferences
+                editor.putString("name", "").apply();
+                editor.putString("switch", "").apply();
+            }
+
             //if statement to see if any of the strings are empty, if they are create toast to inform user
             //If none are empty, open email and fill in fields
             if (emptyEmail(name, email, subject, message))
@@ -96,4 +140,5 @@ public class contact_frag extends Fragment implements View.OnClickListener
     {
         return name.equals("") || email.equals("") || subject.equals("") || message.equals("");
     }
+
 }
